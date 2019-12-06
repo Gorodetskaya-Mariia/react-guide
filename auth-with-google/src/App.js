165b384aee447e611d9879721cdd1005b923eafd
Gorodetskaya-Mariia <1907mss@gmail.com>
2019-12-06 15:22:44 +0300

    authentication with google has been added

---
+++ auth-with-google/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GoogleAuth from "./components/GoogleAuth";
+
+function App() {
+  return (
+      <header className="App-header">
+				<GoogleAuth></GoogleAuth>
+			</header>
+  );
+}
+
+export default App;
---
+++ auth-with-google/src/components/GoogleAuth.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { connect } from "react-redux";
+import { signIn, signOut } from "../action/auth";
+
+class GoogleAuth extends React.Component {
+
+	componentDidMount(){
+		window.gapi.load("client:auth2", ()=> {
+			window.gapi.client.init({
+				clientId: "137234967081-9jjfg5fdu62eb40jpkemvf57mbtrrb8t.apps.googleusercontent.com",
+				scope: "email"
+			}).then(()=>{
+				this.auth = window.gapi.auth2.getAuthInstance();
+				this.onAuthChange(this.auth.isSignedIn.get());
+				this.auth.isSignedIn.listen(this.onAuthChange);
+			});
+		});
+	}
+
+	onAuthChange = isSignedIn => {
+		if(isSignedIn){
+			this.props.signIn(this.auth.currentUser.get().getId());
+		} else {
+			this.props.signOut();
+		}
+	}
+
+	onSignInClick = () =>{
+		this.auth.signIn();
+	};
+
+	onSignOutClick = () =>{
+		this.auth.signOut();
+	};
+
+	renderAuthButton(){
+		if(this.props.isSignedIn === null){
+			return null;
+		} else if(this.props.isSignedIn){
+			return <button onClick={ this.onSignOutClick }>Sign Out with Google</button>
+		} else {
+			return <button onClick={ this.onSignInClick }>Sign In with Google</button>
+		}
+	}
+
+	render(){
+	return <div>{ this.renderAuthButton()}</div>
+	}
+}
+
+const mapStateToProps = (state) => {
+	return { isSignedIn: state.auth.isSignedIn };
+};
+
+export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth);
