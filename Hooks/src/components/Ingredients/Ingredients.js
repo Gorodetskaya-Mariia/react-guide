d5799cf9f614590a9e292a777b897f094e082e88
Gorodetskaya-Mariia <1907mss@gmail.com>
2020-02-09 18:03:42 +0300

    fetching data from firebase has been done

--- Hooks/src/components/Ingredients/Ingredients.js
+++ Hooks/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -6,6 +6,22 @@ import Search from './Search';
 
 function Ingredients() {
   const [userIngredients, setIngredient] = useState([]);
+
+  useEffect(() => {
+    fetch('https://react-hooks-a15ed.firebaseio.com/ingredients.json')
+      .then(response => response.json())
+      .then(responseData => {
+        const data = [];
+        for (let key in responseData) {
+          data.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          })
+        }
+        setIngredient(data);
+      })
+  }, []);
 
   const addIngredientHandler = ingredient => {
     fetch('https://react-hooks-a15ed.firebaseio.com/ingredients.json', {
